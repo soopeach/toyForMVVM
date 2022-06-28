aae756908e231da0b0e6bb906784bd646d5befd0
soopeach <hsjeon584@gmail.com>
2022-06-28 18:45:42 +0900

    Feat: 검색 결과가 없다면 토스트로 경고문구 출력기능 추가

--- app/src/main/java/com/soopeach/movietoyproject/rank/RankFragment.kt
+++ app/src/main/java/com/soopeach/movietoyproject/rank/RankFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.soopeach.movietoyproject.R
 import com.soopeach.movietoyproject.adapters.RankListAdapter
@@ -24,6 +26,14 @@ class RankFragment : Fragment() {
         binding.setLifecycleOwner(this)
         val rankViewModel = ViewModelProvider(this).get(RankViewModel::class.java)
         binding.viewModel = rankViewModel
+
+        rankViewModel.listEmpty.observe(viewLifecycleOwner, Observer { isEmpty ->
+            if (isEmpty){
+                Toast.makeText(context, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
+                rankViewModel.listInit()
+            }
+        })
+
 
         val adapter = RankListAdapter()
         binding.recyclerView.adapter = adapter
--- app/src/main/java/com/soopeach/movietoyproject/rank/RankViewModel.kt
+++ app/src/main/java/com/soopeach/movietoyproject/rank/RankViewModel.kt
@@ -16,19 +16,25 @@ import java.time.format.DateTimeFormatter
 
 class RankViewModel : ViewModel() {
 
+    // 오늘 날짜
     val today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
 
+    // 일별 박스오피스 순위를 검색할 날짜 (20220622 형식)
     val _dateForSearch = MutableLiveData<String>()
     val dateForSearch : LiveData<String>
         get() = _dateForSearch
 
+    // 일별 박스오피스 1 ~ 10위의 영화들을 남을 리스트
     private val _movieRankList = MutableLiveData<List<RankList.BoxOfficeResult.DailyBoxOffice>>()
     val movieRankList : LiveData<List<RankList.BoxOfficeResult.DailyBoxOffice>>
         get() = _movieRankList
 
+    private val _listEmpty = MutableLiveData<Boolean>()
+    val listEmpty : LiveData<Boolean>
+        get() = _listEmpty
+
     init {
-        // 검색을 시작하기위한 기본값은 오늘 날짜
-//        _dateForSearch.value = today
+        listInit()
         Log.d("RankViewModel", "${dateForSearch.value}")
     }
 
@@ -42,8 +48,15 @@ class RankViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val rankList = retrofitForRankList.getRankData(targetDt = dateForSearch.value ?: today)
-                _movieRankList.value = rankList.getRankList()
-                Log.d("통신성공-rank","${_movieRankList.value}")
+
+                // 검색 정보가 없다면
+                if (rankList.getRankList().isEmpty()){
+                    Log.d("통신성공-rank","검색 정보가 없음.")
+                    _listEmpty.value = true
+                } else {
+                    _movieRankList.value = rankList.getRankList()
+                    Log.d("통신성공-rank","${_movieRankList.value}")
+                }
 
             } catch (e: Exception){
                 Log.d("통신실패-rank", "$e 에러발생")
@@ -57,5 +70,8 @@ class RankViewModel : ViewModel() {
 
     // 검색창 초기화
     fun clearSearchBar(){ _dateForSearch.value = ""}
+
+    // listIsEmpty false로 초기화
+    fun listInit(){ _listEmpty.value = false }
 
 }
